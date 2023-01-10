8ce8020b4ad437f7e6ec0c8f3fc3a0b9c8993e94
Kaushal Prajapati <98817537+Kaushalprajapti@users.noreply.github.com>
2023-01-11 01:29:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const text=document.querySelector("textarea"),
 speechBtn=document.querySelector("button");
+const v=document.getElementById("voice").value;
+function getVoices() {
+    let voices = speechSynthesis.getVoices();
+    if(!voices.length){
+      let utterance = new SpeechSynthesisUtterance("");
+      speechSynthesis.speak(utterance);
+      voices = speechSynthesis.getVoices();
+    }
+    return voices;
+  }
 
 function textTospeech(text1){
     let speak=new SpeechSynthesisUtterance(text1);
+    speak.rate = rate.value;
+    speak.voice=getVoices()[voice.value];
     speechSynthesis.speak(speak);
 }
 
